60149b454ae1273b4a73838be45a47fbcdcd6cd1
Agnivesh Adhikari <agnivesh.adhikari@gmail.com>
2018-10-06 04:54:42 +0530

    Added idea TODOs

--- pcg/preprocessing.py
+++ pcg/preprocessing.py
@@ -1,5 +1,11 @@
 # TODO 
 # 1. Normalization of amplitude
+
 # 2. Low pass filter
 # 3. High pass filter
 # 4. Band-pass filter
+# TODO: Find out if 2, 3, 4 reduce the time to compute the spectrogram, or
+#       it makes more sense to just slice the spectrogram instead of using
+#       frequency domain filters.
+
+# 5. Center data around zero and normalize scaling accross axes
