d39ea3a6393f3ad5c99b87a2aa28060c5dcd423f
Agnivesh Adhikari <agnivesh.adhikari@gmail.com>
2018-10-10 23:06:32 +0530

    Added package mlutils. Added ocde for oversampling. Not using it yet.

---
+++ mlutils/classbalancing.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+from numpy import unique, random
+import pandas as pd
+
+def oversample(dataframe, column):
+    classes, counts = unique(dataframe[column], return_counts=True)
+    max_size = max(counts)
+    additional_df = pd.DataFrame()
+    indices_to_repeat = list()
+
+    for c in classes:
+        matching_df = dataframe[dataframe[column] == c]
+        count = len(matching_df)
+        deficit = max_size - count
+
+        if deficit == 0:
+            continue
+
+        # Selecting 'deficit' number of random data points to repeat
+        # replace=False would ensure the same sample is not added too many times,
+        # but then the case where count < deficit would have to be handled separately
+        indices_to_repeat_for_c = list(random.choice(matching_df.index, deficit, replace=True))
+        indices_to_repeat.extend(indices_to_repeat_for_c)
+
+
+    oversampled_dataframe = dataframe.append(dataframe.iloc[indices_to_repeat, :], ignore_index=True)
+
+    return oversampled_dataframe
+
+
