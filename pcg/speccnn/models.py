2fd64f0784d2da67f860e5404f14acfa182b6a3d
Agnivesh Adhikari <agnivesh.adhikari@gmail.com>
2019-03-23 16:07:30 +0530

    Slightly more sensible way to test for improvements

--- pcg/speccnn/models.py
+++ pcg/speccnn/models.py
@@ -1,7 +1,19 @@
 from keras import Model, Sequential
 from keras.layers import Conv1D, MaxPool1D, LSTM, Dense, BatchNormalization
 
+# Because we can just patch as many conv blocks before the recurrent part,
+# we start off with a baseline of zero conv blocks.
+
 def model_A(input_shape):
+    return Sequential(layers=[
+        BatchNormalization(input_shape=input_shape),
+
+        LSTM(20, return_sequences=True, dropout=0.5),
+        LSTM(10),
+        Dense(1, activation='sigmoid')
+    ])
+
+def model_B(input_shape):
     return Sequential(layers=[
         BatchNormalization(input_shape=input_shape),
 
@@ -18,15 +30,7 @@ def model_A(input_shape):
         Conv1D(64, kernel_size=5, strides=1),
         MaxPool1D(pool_size=2),
 
-        Conv1D(64, kernel_size=5, strides=1),
-        Conv1D(64, kernel_size=5, strides=1),
-        MaxPool1D(pool_size=2),
-
-        Conv1D(64, kernel_size=5, strides=1),
-        Conv1D(64, kernel_size=5, strides=1),
-        MaxPool1D(pool_size=2),
-
-        LSTM(20, return_sequences=True),
+        LSTM(20, return_sequences=True, dropout=0.5),
         LSTM(10),
         Dense(1, activation='sigmoid')
     ])
